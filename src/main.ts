6e5de46259b7b1201ea691efa0d313eb27e08017
Raul Aquino <aquinoraul207@gmail.com>
2025-10-13 23:09:40 -0300

    added swagger and health endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Ridiculously Complex User CRUD')
+      .setDescription('The user API description')
+      .setVersion('1.0')
+      .addTag('users')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
