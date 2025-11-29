2cfcf305269d5d19dffa99a162e2fba6820a2761
Raul Aquino <aquinoraul207@gmail.com>
2025-11-29 11:24:46 -0300

    added localstack and removed vault just for while

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,10 @@ async function bootstrap() {
 
     const documentFactory = () => SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, documentFactory);
+    Logger.warn(
+      `Aplication is running in ${process.env.NODE_ENV} mode. Documentation available at http://localhost:${process.env.PORT ?? 3000}/api`,
+      'Bootstrap',
+    );
   }
 
   await app.listen(process.env.PORT ?? 3000);
